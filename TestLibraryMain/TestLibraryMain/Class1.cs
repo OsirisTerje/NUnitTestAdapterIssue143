c79695a6d664da2b6c5df724f9b4a36e1709feda
Andrew Kvochick <andrew.kvochick@gmail.com>
2017-03-21 12:11:47 -0400

    initial import: working test

---
+++ TestLibraryMain/TestLibraryMain/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibraryMain
+{
+  [TestFixture]
+  public class Class1
+  {
+    [Test]
+    public void TestTruth()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
