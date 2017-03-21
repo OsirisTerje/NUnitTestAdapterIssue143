6bc745d1c835a7107d0ceb5da0149151a72c725b
Andrew Kvochick <andrew.kvochick@gmail.com>
2017-03-21 12:26:31 -0400

    adds second test project: still works

---
+++ TestLibraryMain/OtherTestLibrary/OtherDummyTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherTestLibrary
+{
+  [TestFixture]
+  public class OtherDummyTest
+  {
+    [Test]
+    public void FalseTest()
+    {
+      Assert.IsFalse(false);
+    }
+  }
+}
