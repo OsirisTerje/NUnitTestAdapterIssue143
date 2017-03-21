e0d53b433f242981cd1388ace975756dffd98949
Terje Sandstrom <terje@hermit.no>
2017-03-21 20:38:51 +0100

    removed comments

--- TestLibraryMain/OtherTestLibrary/OtherDummyTest.cs
+++ TestLibraryMain/OtherTestLibrary/OtherDummyTest.cs
@@ -1,15 +1,9 @@
 ﻿using NUnit.Framework;
-//using SimulatedPrecompiledCommonLibrary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OtherTestLibrary
 {
   [TestFixture]
-  public class OtherDummyTest // : IntegrationTestBaseClass
+  public class OtherDummyTest 
   {
     [Test]
     public void FalseTest()
