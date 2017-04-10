943d898f5a1911246ad62dc8d4343ca25b84e0ed
Terje Sandstrom <terje@hermit.no>
2017-04-10 18:56:30 +0200

    Updated with test in derived class

--- TestLibraryMain/TestLibraryMain/InheritsFromOtherDummyTest.cs
+++ TestLibraryMain/TestLibraryMain/InheritsFromOtherDummyTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TestLibraryMain
 {
@@ -13,5 +14,11 @@ namespace TestLibraryMain
 
     public class InheritsFromOtherDummyTestOnly : OnlyHereInOtherTest
     {
+        [Test]
+        public void TestInInheritedClass()
+        {
+            Assert.Pass();
+        }
+
     }
 }
